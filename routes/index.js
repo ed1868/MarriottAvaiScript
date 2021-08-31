a2b3d5826c23aa10c40df2001cd04bf23c9f83d1
ed1868 <ruizeduardo21@gmail.com>
2021-08-31 13:10:54 -0400

    extracted marriott code and pushed to new list

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 
+const MarriottDerbyHotels = require('../seeds/hotels/MarriotDerbyList');
 /* GET home page. */
+
+const marriottCodeList = [];
+
+MarriottDerbyHotels.map(hotel => {
+  if (hotel.hotelId) {
+    marriottCodeList.push(hotel.hotelId)
+  } else {
+    console.log('nope')
+  }
+});
+
+
 router.get('/', (req, res, next) => {
-  res.render('index');
+
+  console.log('TOTAL CODE LIST : ', marriottCodeList.length);
+  res.status(200).json(marriottCodeList);
 });
 
 module.exports = router;
