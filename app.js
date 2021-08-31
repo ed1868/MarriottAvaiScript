c845c76a638d15efd78de549fe8bd30034a536f8
ed1868 <ruizeduardo21@gmail.com>
2021-08-31 13:50:31 -0400

    making succesfull api calls and getting room rates

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express      = require('express');
-const path         = require('path');
-const favicon      = require('serve-favicon');
-const logger       = require('morgan');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
 const cookieParser = require('cookie-parser');
-const bodyParser   = require('body-parser');
-
-
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const MarriottHotelCodeList = require('./seeds/hotels/MarriottHotelCodeList');
 const app = express();
 
 // view engine setup
@@ -43,5 +43,114 @@ app.use((err, req, res, next) => {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
+const testArr = [
+  "ABYFX",
+  "ABVSI"
+]
+
+const succesfulQueries = [
+
+]
+
+const unsuccesfulQueries = [
+
+]
+queryHotel = (id, checkin, checkout) => {
+  console.log(`HOTEL ${id} IS BEING QUERIED`);
+  console.log(`HOTEL CHECK IN : ${checkin} `);
+  console.log(`HOTEL CHECK OUT : ${checkout} `);
+
+
+  axios({
+    method: "post",
+    url: "https://go3-tn4-aws.derbysoftca.com/api/go/bookingusb/v4/availability",
+    headers: {
+      Authorization:
+        "2Rrae9SU2G50R3ZmGJ0000011dfc52488ecb444a8b4a1eda945f0f4c",
+    },
+    data: {
+      "header": {
+        "supplierId": "MARRIOTT",
+        "distributorId": "OVERSEAS",
+        "version": "v4.0",
+        "token": "1630430035483"
+      },
+      "stayRange": {
+        "checkin": checkin,
+        "checkout": checkout
+      },
+      "roomCriteria": {
+        "roomCount": 1,
+        "adultCount": 1,
+        "childCount": 0,
+        "childAges": []
+      },
+      "hotelId": id,
+      "iata": "",
+      "promoteCode": "",
+      "isAfterPromotion": false
+    }
+  })
+    .then((payload) => {
+      if (payload.status == "200") {
+        if (payload.data.roomRates.length >= 1) {
+          succesfulQueries.push({
+            hotelId: payload.data.hotelId, roomRates: payload.data.roomRates
+          })
+
+
+        } else {
+          console.log(payload.data)
+        }
+
+      }
+      console.log('succesful hotel queries ::::: ', succesfulQueries.length);
+    })
+    .catch((err) => {
+      unsuccesfulQueries.push(id);
+      if (err && err.code == "ETIMEDOUT") {
+        console.log(':::::::::::::UNDEFINED ERROR :::::::::::::: ', err)
+        setTimeout(function () {
+          console.log(':::::::::::::::ERROR FIX SET TIME OUT ID :::::::::::::::::::::::::::::::', id)
+          queryHotel(id, checkin, checkout)
+        }, 6000)
+
+      }
+      if (err && err.response.status) {
+        if (err.response.status == "429") {
+          setTimeout(function () {
+            console.log(':::::::::::::::ERROR FIX SET TIME OUT ID :::::::::::::::::::::::::::::::', id)
+            queryHotel(id, checkin, checkout)
+          }, 6000)
+        }
+      }
+      else {
+        console.log('THE FUCKING BIG ERROR: ', err)
+      }
+
+
+      console.log('unsuccesful hotel queries :::::: ', unsuccesfulQueries)
+    });
+}
+
+
+async function getQuery(hotelId) {
+  let checkin = "2021-09-10";
+  let checkout = "2021-09-12";
+  await queryHotel(hotelId, checkin, checkout)
+}
+
+
+
+async function getAllHotels() {
+  const apiPromises = MarriottHotelCodeList.map(getQuery)
+
+  await Promise.all(apiPromises)
+}
+
+getAllHotels()
+
 
 module.exports = app;
